178f5b56470319ed9c8990e78facb7df46d2b0c5
Yves Habersaat <yveshabersaat@hotmail.com>
2021-04-09 16:29:59 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,3 +5,15 @@ const build = require('@microsoft/sp-build-web');
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
 build.initialize(gulp);
+
+build.configureWebpack.mergeConfig({
+    additionalConfiguration: (generatedConfiguration) => {
+      generatedConfiguration.module.rules.push(
+        {
+            test: /\.(woff2)$/,
+            loader: 'url-loader?limit=100000'
+        }
+      );
+      return generatedConfiguration;
+    }
+  });
